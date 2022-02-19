54d6eb568dad5852ae12094a6372ba9a2fb23148
TomoyaMizukuro <salvatoleyohane@gmail.com>
2022-02-19 13:17:52 +0900

    UPDATE 不要なコード削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import HomeView from "../views/HomeView.vue";
 import TopView from "../views/Top.vue";
 import SupportsView from "../views/Supports.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "home",
-  //   component: HomeView,
-  // },
   {
     path: "/",
     name: "top",
@@ -26,15 +20,6 @@ const routes = [
     path: "/hosts",
     name: "hosts",
     component: SupportsView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
